f6ef58525b26ca791da85730e1987f91a9dd1b4a
kssadomtsev <sadomcev.k@gmail.com>
2019-04-08 11:10:58 +0300

    HW2. Correction #5

--- src/com/mysite/webapp/model/Resume.java
+++ src/com/mysite/webapp/model/Resume.java
@@ -24,8 +24,12 @@ public class Resume {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o){
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
         Resume resume = (Resume) o;
         return getUuid().equals(resume.getUuid());
     }
