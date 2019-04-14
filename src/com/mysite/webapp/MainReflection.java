cdb0815d6ac3144e81caecf03b72d3e3ae1aa426
kssadomtsev <sadomcev.k@gmail.com>
2019-04-14 22:36:10 +0300

    Lesson 4. Added reflection and JUnit

---
+++ src/com/mysite/webapp/MainReflection.java
@@ -0,0 +1,22 @@
+package com.mysite.webapp;
+
+import com.mysite.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
+        System.out.println(r);
+        Method methodToString = r.getClass().getMethods()[1];
+        System.out.println(methodToString.invoke(r));
+    }
+}
