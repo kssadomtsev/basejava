b3382b43e10b614325dd9c03c9d9a11d6c6ac47c
kssadomtsev <sadomcev.k@gmail.com>
2019-06-03 23:01:04 +0300

    Lesson 10. HW 2

--- src/com/mysite/webapp/storage/strategies/DataStreamIOStrategy.java
+++ src/com/mysite/webapp/storage/strategies/DataStreamIOStrategy.java
@@ -5,6 +5,7 @@ import com.mysite.webapp.model.*;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -15,36 +16,41 @@ public class DataStreamIOStrategy implements IOStrategy {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
             Map<ContactType, String> contacts = r.getContacts();
-            writeMap(dos, contacts, (d, data) -> d.writeUTF((String) data));
+            writeCollection(dos, contacts.entrySet(), data -> {
+                dos.writeUTF(data.getKey().name());
+                dos.writeUTF(data.getValue());
+            });
             Map<SectionType, AbstractSection> sections = r.getSections();
-            writeMap(dos, sections, (d, data) -> {
-                String className = data.getClass().getSimpleName();
-                d.writeUTF(className);
-                switch (className) {
-                    case "TextSection":
-                        TextSection textSection = (TextSection) data;
-                        d.writeUTF(textSection.getContent());
+            writeCollection(dos, sections.entrySet(), data -> {
+                SectionType sectionType = data.getKey();
+                AbstractSection section = data.getValue();
+                dos.writeUTF(sectionType.name());
+                switch (sectionType) {
+                    case PERSONAL:
+                    case OBJECTIVE:
+                        TextSection textSection = (TextSection) section;
+                        dos.writeUTF(textSection.getContent());
                         break;
-                    case "ListSection":
-                        ListSection listSection = (ListSection) data;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        ListSection listSection = (ListSection) section;
                         List<String> contentList = listSection.getContentList();
-                        writeList(d, contentList, (d1, data1) -> d1.writeUTF((String) data1));
+                        writeCollection(dos, contentList, dos::writeUTF);
                         break;
-                    case "OrganizationSection":
-                        OrganizationSection organizationSection = (OrganizationSection) data;
+                    case EXPERIENCE:
+                    case EDUCATION:
+                        OrganizationSection organizationSection = (OrganizationSection) section;
                         List<Organization> organizationList = organizationSection.getOrganizationList();
-                        writeList(d, organizationList, (d1, data1) -> {
-                            Organization organization = (Organization) data1;
+                        writeCollection(dos, organizationList, organization -> {
                             Link link = organization.getLinkOrganization();
-                            d1.writeUTF(link.getTitle());
-                            d1.writeUTF(link.getURL());
+                            dos.writeUTF(link.getTitle());
+                            dos.writeUTF(link.getURL() != null ? link.getURL() : "null");
                             List<Organization.Position> positionList = organization.getPositionList();
-                            writeList(d1, positionList, (d2, data2) -> {
-                                Organization.Position position = (Organization.Position) data2;
-                                d2.writeUTF(position.getStartDate().toString());
-                                d2.writeUTF(position.getEndDate().toString());
-                                d2.writeUTF(position.getTitle());
-                                d2.writeUTF(position.getDescription());
+                            writeCollection(dos, positionList, position -> {
+                                dos.writeUTF(position.getStartDate().toString());
+                                dos.writeUTF(position.getEndDate().toString());
+                                dos.writeUTF(position.getTitle());
+                                dos.writeUTF(position.getDescription() != null ? position.getDescription() : "null");
                             });
                         });
                         break;
@@ -59,28 +65,37 @@ public class DataStreamIOStrategy implements IOStrategy {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            readMap(dis, resume, (r, d) -> r.setContact(ContactType.valueOf(d.readUTF()), d.readUTF()));
-            readMap(dis, resume, (r, d) -> {
-                SectionType sectionType = SectionType.valueOf(d.readUTF());
-                String className = d.readUTF();
-                switch (className) {
-                    case "TextSection":
-                        TextSection textSection = new TextSection(d.readUTF());
-                        r.setSection(sectionType, textSection);
+            readCollection(dis, () -> resume.setContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            readCollection(dis, () -> {
+                SectionType sectionType = SectionType.valueOf(dis.readUTF());
+                switch (sectionType) {
+                    case PERSONAL:
+                    case OBJECTIVE:
+                        TextSection textSection = new TextSection(dis.readUTF());
+                        resume.setSection(sectionType, textSection);
                         break;
-                    case "ListSection":
-                        ListSection listSection = new ListSection(readList(d, DataInput::readUTF));
-                        r.setSection(sectionType, listSection);
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        ListSection listSection = new ListSection(getList(dis, dis::readUTF));
+                        resume.setSection(sectionType, listSection);
                         break;
-                    case "OrganizationSection":
-                        OrganizationSection organizationSection = new OrganizationSection(readList(d, d1 -> {
-                            Link link = new Link(d1.readUTF(), d1.readUTF());
-                            List<Organization.Position> positionList = readList(d1, d2 ->
-                                    new Organization.Position(LocalDate.parse(d2.readUTF()),
-                                            LocalDate.parse(d2.readUTF()), d2.readUTF(), d2.readUTF()));
+                    case EXPERIENCE:
+                    case EDUCATION:
+                        OrganizationSection organizationSection = new OrganizationSection(getList(dis, () -> {
+                            String urlTitle = dis.readUTF();
+                            String url = dis.readUTF();
+                            Link link = new Link(urlTitle, url.equals("null") ? null : url);
+                            System.out.println(link);
+                            List<Organization.Position> positionList = getList(dis, () -> {
+                                LocalDate startDate = LocalDate.parse(dis.readUTF());
+                                LocalDate endDate = LocalDate.parse(dis.readUTF());
+                                String title = dis.readUTF();
+                                String description = dis.readUTF();
+                                return new Organization.Position(startDate, endDate, title, description.equals("null") ? null : description);
+                            });
                             return new Organization(link, positionList);
                         }));
-                        r.setSection(sectionType, organizationSection);
+                        resume.setSection(sectionType, organizationSection);
                         break;
                 }
             });
@@ -88,46 +103,39 @@ public class DataStreamIOStrategy implements IOStrategy {
         }
     }
 
-    private interface WriterUTF<T> {
-        void writeUTF(DataOutputStream dos, T data) throws IOException;
+    private interface Writer<T> {
+        void writeData(T data) throws IOException;
     }
 
-    private <K extends Enum<K>, V> void writeMap(DataOutputStream dos, Map<K, V> map, WriterUTF writerUTF) throws IOException {
-        dos.writeInt(map.size());
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            dos.writeUTF(entry.getKey().name());
-            writerUTF.writeUTF(dos, entry.getValue());
+    private <T> void writeCollection(DataOutputStream dos, Collection<T> collection, Writer<T> writer) throws IOException {
+        dos.writeInt(collection.size());
+        for (T entry : collection) {
+            writer.writeData(entry);
         }
     }
 
-    private <T> void writeList(DataOutputStream dos, List<T> list, WriterUTF writerUTF) throws IOException {
-        dos.writeInt(list.size());
-        for (T entry : list) {
-            writerUTF.writeUTF(dos, entry);
-        }
+    private interface Reader<T> {
+        T readData() throws IOException;
+
     }
 
-    private interface ReaderUTF<T> {
-        T readUTF(DataInputStream dis) throws IOException;
-    }
-
-    private <T> List<T> readList(DataInputStream dis, ReaderUTF readerUTF) throws IOException {
+    private <T> List<T> getList(DataInputStream dis, Reader<T> reader) throws IOException {
         int size = dis.readInt();
         List<T> list = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
-            list.add((T) readerUTF.readUTF(dis));
+            list.add(reader.readData());
         }
         return list;
     }
 
     private interface FillerResume {
-        void fillResume(Resume resume, DataInputStream dis) throws IOException;
+        void fillResume() throws IOException;
     }
 
-    private void readMap(DataInputStream dis, Resume resume, FillerResume fillerResume) throws IOException {
+    private void readCollection(DataInputStream dis, FillerResume fillerResume) throws IOException {
         int size = dis.readInt();
         for (int i = 0; i < size; i++) {
-            fillerResume.fillResume(resume, dis);
+            fillerResume.fillResume();
         }
     }
 }
